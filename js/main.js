8ab58f067d9c05fdabb0e72b2bd94eece7eb709d
Santiago Pereira <s.pereira.insua@gmail.com>
2024-08-22 21:29:56 -0300

    Clase semana 1

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+//Declaración de constantes
+const academia = "CoderHouse";
+
+//Declaración de variables
+let curso = prompt("Bienvenido! Que materia estas cursando? ");
+let nro_comision = prompt("Ingresa tu número de comisión: ");
+//let academia = prompt("En que academia estas cursando? ");
+//let flex = true;
+
+
+//Mostrar en Consola
+// console.log('Muestra contenido en la consola del navegador');
+//console.log("Hola! Esta es la comision " + nro_comision + " de la academia " + academia);
+
+//Mensaje de alerta
+alert("Hola! Esta es la comision " + nro_comision + " del curso de " + curso + " de la academia " + academia);
+
+let numeroA = 60;
+let numeroB = 45;
+//parseInt convierte de string a entero, parseFloat convierte string a tipo flotante
+let resultado = parseFloat(numeroA + numeroB);
+resultado = numeroA - numeroB;
+resultado = numeroA * numeroB;
+resultado = numeroA / numeroB;
