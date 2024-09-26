ae78b4d1ea0fede9572af633cebddd58461d37e7
Santiago Pereira <s.pereira.insua@gmail.com>
2024-09-26 20:58:32 -0300

    Clase sobre funciones de orden superior

--- js/main.js
+++ js/main.js
@@ -169,52 +169,168 @@
 //     this.marca = marca
 //     this.precio = precio
 // }
-const precioDolar = 40
-class Celular {
-    static id = 0
-    constructor (modelo, marca, precio) {
-        this.id = ++Celular.id
-        this.modelo = modelo
-        this.marca = marca
-        this.precio = precio
+
+// const precioDolar = 40
+// class Celular {
+//     static id = 0
+//     constructor (modelo, marca, precio) {
+//         this.id = ++Celular.id
+//         this.modelo = modelo
+//         this.marca = marca
+//         this.precio = precio
+//     }
+//     enPesos = () => {
+//         return this.precio * precioDolar
+//     }
+// }
+
+// const celular1 = new Celular("S24", "Samsung", 1000)
+// const celular2 = new Celular("Iphone 16", "Apple", 1900)
+
+// console.log(celular1, celular2)
+
+// const productos = []
+
+// const cargarProductos = () => {
+//     let cargarModelo = prompt("Ingrese el modelo del teléfono ")
+//     let cargaMarca = prompt("Ingrese la marca del teléfono ")
+//     let cargaPrecio = prompt("Ingrese el precio en dolares ")
+
+//     const celular = new Celular(cargarModelo, cargaMarca, cargaPrecio)
+//     productos.push(celular)
+// }
+
+// const verProductos = () => {
+//     console.log(productos)
+// }
+
+// let menu = parseInt(prompt("Ingrese la opción:\n 1- Ver catalogo de productos\n 2- Cargar nuevo producto\n 3- Salir"))
+// while (menu =! 3) {
+//     switch (menu) {
+//         case 1:
+//             verProductos()
+//             break
+//         case 2:
+//             cargarProductos()
+//             break
+//         default:
+//             alert("Opción invalida")
+//             break
+//     }
+//     menu = parseInt(prompt("Ingrese la opción:\n 1- Ver catalogo de productos\n 2- Cargar nuevo producto\n 3- Salir"))
+// }
+
+//CLASE FUNCIONES DE ORDEN SUPERIOR
+
+// let numeroElegido = parseInt(prompt("Ingrese el número elegido a multiplicar: "))
+
+// function multiplicador (numero) {
+//     return (numero2) => numero2*numero
+// }
+
+// let resultado = multiplicador(10)
+// console.log(resultado(numeroElegido))
+
+
+
+// let operacion = prompt("Ingrese si desea multiplicar o dividir: ")
+// let numero1 = parseInt(prompt("Ingrese el primer número: "))
+// let numero2 = parseInt(prompt("Ingrese el segundo número: "))
+// function elegirOperacion(operacion) {
+//     if (operacion == "multiplicar") {
+//         return (a, b) => a * b
+//     } else if (operacion == "dividir") {
+//         return (a, b) => a / b
+//     }
+// }
+
+// let operacionElegida = elegirOperacion(operacion)
+// console.log(operacionElegida(numero1,numero2))
+
+
+// const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// function numerador (numeros, visualizar) {
+//     for (const numero of numeros) {
+//         visualizar(numero)
+//     }
+// }
+
+// // numerador(numeros, console.log)
+// function multiplicar(num) {
+//     let multiplicado = num*10
+//     console.log(multiplicado)
+// }
+// //numerador(numeros, multiplicar)
+// const tablaDel10 = []
+// numerador(numeros, (numero) => {
+//     tablaDel10.push(numero*10)
+// })
+// console.log(tablaDel10)
+
+
+
+const productos = [
+    {
+        id: 1,
+        nombre: "televisor",
+        precio: 5000
+    },
+    {
+        id: 2,
+        nombre: "lavarropas",
+        precio: 8000
+    },
+    {
+        id: 3,
+        nombre: "microondas",
+        precio: 2000
+    },
+    {
+        id: 4,
+        nombre: "secadora",
+        precio: 4000
+    },
+    {
+        id: 5,
+        nombre: "cocina",
+        precio: 13000
     }
-    enPesos = () => {
-        return this.precio * precioDolar
+]
+
+//forEach()
+productos.forEach((producto) => {
+    //console.log(producto)
+    console.log("Producto: " + producto.nombre + " Precio: " + producto.precio)
+})
+
+//find() devuelve el primer objeto que encuentra
+const buscar = prompt("Ingrese el producto a buscar: ")
+const busqueda = productos.find((producto) => producto.nombre == buscar)
+console.log(busqueda)
+
+//filter()
+let filtro = parseInt(prompt("Ingrese el precio máximo a mostrar: "))
+const filtrados = productos.filter((producto) => producto.precio <= filtro)
+console.log(filtrados)
+
+//some()
+console.log(productos.some((producto) => producto.nombre == "heladera"))
+
+//map()
+const productosDisponibles = productos.map((producto) => producto.nombre)
+console.log(productosDisponibles)
+
+const descuento = productos.map((producto) => {
+    let precioDescuento = producto.precio - (producto.precio*20/100)
+    return {
+        nombre: producto.nombre,
+        precio: precioDescuento
     }
-}
+})
+console.log(descuento)
 
-const celular1 = new Celular("S24", "Samsung", 1000)
-const celular2 = new Celular("Iphone 16", "Apple", 1900)
+//reduce() suma total de precio de objetos
+const total = productos.reduce((contador, producto) => contador + producto.precio, 0)
+console.log(total)
 
-console.log(celular1, celular2)
-
-const productos = []
-
-const cargarProductos = () => {
-    let cargarModelo = prompt("Ingrese el modelo del teléfono ")
-    let cargaMarca = prompt("Ingrese la marca del teléfono ")
-    let cargaPrecio = prompt("Ingrese el precio en dolares ")
-
-    const celular = new Celular(cargarModelo, cargaMarca, cargaPrecio)
-    productos.push(celular)
-}
-
-const verProductos = () => {
-    console.log(productos)
-}
-
-let menu = parseInt(prompt("Ingrese la opción:\n 1- Ver catalogo de productos\n 2- Cargar nuevo producto\n 3- Salir"))
-while (menu =! 3) {
-    switch (menu) {
-        case 1:
-            verProductos()
-            break
-        case 2:
-            cargarProductos()
-            break
-        default:
-            alert("Opción invalida")
-            break
-    }
-    menu = parseInt(prompt("Ingrese la opción:\n 1- Ver catalogo de productos\n 2- Cargar nuevo producto\n 3- Salir"))
-}
