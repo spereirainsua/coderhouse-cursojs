b18df848aa71b997aa34b91b9c9ce06e0da114a4
Santiago Pereira <s.pereira.insua@gmail.com>
2024-10-08 20:53:25 -0300

    Checkpoint 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,71 @@
-
-//TEST DE FUNCIONES
-// const testDeFunciones = () => {
-//     solicitarValores()
-//     let continuar = true
-//     while (continuar) {
-//         let menu = parseInt(prompt("Menu para testear las funciones\nSeleccione la opción deseada: \n   1- mostrarDatos()\n   2- cargarIngresosOGastos()\n   3- sumarIngresos()\n   4- sumarGastos()\n   5- porcentajeGastadoDelSueldo()\n   6- balanceTotal()\n   0- Salir"))
-//         switch (menu) {
-//             case 1:
-//                 mostrarDatos(datosIngresosyGastos)
-//                 break
-//             case 2:
-//                 cargarIngresosOGastos(datosIngresosyGastos)
-//                 break
-//             case 3:
-//                 console.log("Ingresos registrados: " + sumarIngresos(datosIngresosyGastos))
-//                 break
-//             case 4:
-//                 console.log("Gastos registrados: " + sumarGastos(datosIngresosyGastos))
-//                 break
-//             case 5:
-//                 console.log("Gastaste " + Math.round(porcentajeGastadoDelSueldo(30000, datosIngresosyGastos)) + "% de tu sueldo.")
-//             case 6:
-//                 console.log("El balance de tu cuenta es: " + balanceTotal(datosIngresosyGastos))
-//             case 0:
-//                 break
-//             default:
-//                 alert("Opción incorrecta")
-//                 break
-//         }
-//         if (menu == 0) {
-//             let confirmacion = prompt("¿Seguro desea salir? (si/no)").toLowerCase()
-//             if (confirmacion == "si") {
-//                 continuar = false
-//             }
-//         }
-//     }
-// }
-
 //Cargar nuevos montos
 const datosIngresosyGastos = []
+
+//Validación de campos de datos
+const validarNoVacio = (dato) => dato.trim() !== '';
+const validarNumerico = (dato) => !isNaN(dato) && dato.trim() !== '';
+
+class Registro {
+    static id = 0
+    constructor(fecha, concepto, monto, iog) {
+        this.id = ++Registro.id
+        this.fecha = fecha
+        this.concepto = concepto
+        if (iog == "gasto") {
+            monto = monto * -1
+        }
+        this.monto = monto
+    }
+    // enPesos = () => {
+    //     return this.precio * precioDolar
+    // }
+}
+
+//Sumar valores positivos de un array
+const sumarIngresos = (ingresos) => {
+    let total = 0
+    ingresos.forEach(registro => {
+        if (registro.monto > 0) {
+            total += parseFloat(registro.monto)
+        }
+        // console.log(registro.monto)
+    })
+    return total
+}
+
+//Sumar valores negativos de un array
+const sumarGastos = (gastos) => {
+    let total = 0
+    gastos.forEach(registro => {
+        if (registro.monto < 0) {
+            total += parseFloat(registro.monto)
+        }
+    })
+    return total
+}
+
+//Calcular el total de todos los valores ingresados
+const balanceTotal = (datos) => sumarIngresos(datos) + sumarGastos(datos)
+
+const cargarIngresosOGastos = (fecha, concepto, monto, iog) => {
+    let dato = new Registro(fecha, concepto, monto, iog)
+    datosIngresosyGastos.push(dato)
+    localStorage.setItem("Montos", JSON.stringify(datosIngresosyGastos))
+    actualizarDatosMostrados(datosIngresosyGastos)
+}
+
+function obtenerFechaActual() {
+    let fechaActual = new Date();
+    let dia = fechaActual.getDate().toString().padStart(2, '0'); // Agrega un 0 si es necesario
+    let mes = (fechaActual.getMonth() + 1).toString().padStart(2, '0'); // Los meses empiezan en 0
+    let año = fechaActual.getFullYear();
+
+    let horas = fechaActual.getHours().toString().padStart(2, '0');
+    let minutos = fechaActual.getMinutes().toString().padStart(2, '0');
+
+    // Formato DD/MM/AAAA hh:mm
+    return `${dia}/${mes}/${año} ${horas}:${minutos}`;
+}
 
 document.addEventListener("DOMContentLoaded", () => {
     let validarDatos = localStorage.getItem("Montos")
@@ -48,39 +75,46 @@ document.addEventListener("DOMContentLoaded", () => {
         })
         Registro.id = datosIngresosyGastos.slice(-1)[0].id
     }
-    mostrarDatosEnDashboard(datosIngresosyGastos)
+    actualizarDatosMostrados(datosIngresosyGastos)
 })
 
-class Registro {
-    static id = 0
-    constructor(concepto, monto, iog) {
-        this.id = ++Registro.id
-        this.concepto = concepto
-        if (iog == "gasto") {
-            this.monto = monto * -1
-        } else {
-            this.monto = monto
-        }
-    }
-    // enPesos = () => {
-    //     return this.precio * precioDolar
-    // }
-}
 
-const cargarIngresosOGastos = (concepto, monto, iog) => {
-    let dato = new Registro(concepto, monto, iog)
-    datosIngresosyGastos.push(dato)
-    localStorage.setItem("Montos", JSON.stringify(datosIngresosyGastos))
-    mostrarDatosEnDashboard(datosIngresosyGastos)
-}
 
-//Quitar ultimo monto cargado
+
+
+//Quitar ultimo monto cargado ###AUN NO SE USA
 const quitarUltimoMontoCargado = (datosIngresosyGastos) => {
     datosIngresosyGastos.pop()
 }
 
+function decimalAdjust(type, value, exp) {
+    // Si el exp no está definido o es cero...
+    if (typeof exp === "undefined" || +exp === 0) {
+        return Math[type](value);
+    }
+    value = +value;
+    exp = +exp;
+    // Si el valor no es un número o el exp no es un entero...
+    if (isNaN(value) || !(typeof exp === "number" && exp % 1 === 0)) {
+        return NaN;
+    }
+    // Shift
+    value = value.toString().split("e");
+    value = Math[type](+(value[0] + "e" + (value[1] ? +value[1] - exp : -exp)));
+    // Shift back
+    value = value.toString().split("e");
+    return +(value[0] + "e" + (value[1] ? +value[1] + exp : exp));
+}
+
+// Decimal round
+if (!Math.round10) {
+    Math.round10 = function (value, exp) {
+        return decimalAdjust("round", value, exp);
+    };
+}
+
 //Mostrar datos almacenados
-const mostrarDatosEnDashboard = (datos) => {
+const actualizarDatosMostrados = (datos) => {
     let tabla = document.getElementById("tabla-dashboard")
     if (datos.length > 0) {
         if (tabla.classList.contains("centrar-texto-tabla")) {
@@ -89,66 +123,44 @@ const mostrarDatosEnDashboard = (datos) => {
         tabla.innerHTML = `<thead>
                         <tr>
                             <th scope="col">#</th>
+                            <th scope="col">Fecha</th>
                             <th scope="col">Concepto</th>
                             <th scope="col">Monto $</th>
                         </tr>
                     </thead>`
         let bodyTabla = document.createElement("tbody")
+        let numerador = 1
         datos.forEach((dato) => {
-            console.log(dato)
+            // console.log(dato)
             let fila = document.createElement("tr")
-            fila.innerHTML = `<th scope="row">${dato.id}</th>
+            if (dato.monto > 0) {
+                fila.classList.add("fila-positiva")
+            } else {
+                fila.classList.add("fila-negativa")
+            }
+            fila.innerHTML = `<th scope="row">${numerador}</th>
+                            <td>${dato.fecha}</td>
                             <td>${dato.concepto}</td>
-                            <td>${dato.monto}</td>`
-            console.log(fila)
+                            <td>${dato.monto}</td>
+                            <td><button type="button" class="btn btn-outline-dark btn-eliminar-registro" id="${dato.id}"><span class="material-symbols-outlined">close</span></button></td>`
             bodyTabla.append(fila)
+            numerador++
         })
         tabla.append(bodyTabla)
     } else {
         tabla.innerHTML = 'No se encontraron datos.'
         tabla.classList.add("centrar-texto-tabla")
     }
+    let balance = document.getElementById("balance")
+    balance.innerHTML = `$${Math.round10(balanceTotal(datos), -2)}`
 }
 
-//Sumar valores positivos de un array
-const sumarIngresos = (ingresos) => {
-    let total = 0
-    for (let valor of ingresos) {
-        if (valor > 0) {
-            total += valor
-        }
-    }
-    return total
-}
-
-//Sumar valores negativos de un array
-const sumarGastos = (gastos) => {
-    let total = 0
-    for (let valor of gastos) {
-        if (valor < 0) {
-            total += valor
-        }
-    }
-    return total
-}
-
-//Calcular porcentaje gastado a partir de un valor de sueldo
+//Calcular porcentaje gastado a partir de un valor de sueldo ###AUN NO SE USA
 const porcentajeGastadoDelSueldo = (sueldo, datos) => {
     let gastos = sumarGastos(datos) * -1
     let porcentaje = (gastos * 100) / sueldo
     return porcentaje
 }
-
-//Calcular el total de todos los valores ingresados
-const balanceTotal = (datos) => sumarIngresos(datos) + sumarGastos(datos)
-
-
-
-//Llamada a la función de test
-// testDeFunciones()
-
-
-
 
 const collapse = Array.from(document.querySelectorAll('.collapse-carga-balance'))
 const collapse_title = document.getElementById('collapse-title')
@@ -192,15 +204,12 @@ document.addEventListener("click", (event) => {
     }
 })
 
-const validarNoVacio = (dato) => dato.trim() !== '';
-const validarNumerico = (dato) => !isNaN(dato) && dato.trim() !== '';
-
 //INGRESAR NUEVOS DATOS
 btnCargar.addEventListener("click", (e) => {
     let concepto = document.getElementById("concepto").value
     let monto = document.getElementById("monto").value
     if (validarNoVacio(concepto) && validarNumerico(monto)) {
-        cargarIngresosOGastos(concepto, monto, e.currentTarget.name)
+        cargarIngresosOGastos(obtenerFechaActual(), concepto, monto, e.currentTarget.name)
         document.getElementById("concepto").value = ''
         document.getElementById("monto").value = ''
     } else {
